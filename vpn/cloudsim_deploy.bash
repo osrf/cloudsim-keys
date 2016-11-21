d38453666ad56f38b69adcb1dab69554b2e57a78
gerkey <gerkey@osrfoundation.org>
2016-11-21 20:06:05 +0000

    update deploy

--- vpn/cloudsim_deploy.bash
+++ vpn/cloudsim_deploy.bash
@@ -12,15 +12,29 @@ get_option(){
 # This file is created by cloudsim when the machine is launched
 optionsfile=$DIR/cloudsim-options.json
 
-# Get options
+# Common options
 role=`get_option $optionsfile role`
+token=`get_option $optionsfile token`
+# Arbiter options
 blue_route=`get_option $optionsfile blue_route`
 gold_route=`get_option $optionsfile gold_route`
 blue_subnet=`get_option $optionsfile blue_subnet`
 gold_subnet=`get_option $optionsfile gold_subnet`
-token=`get_option $optionsfile token`
+# Payload options
+client_route=`get_option $optionsfile client_route`
+server_ip=`get_option $optionsfile server_ip`
+client_id=`get_option $optionsfile client_id`
 
-if (( $role == "arbiter" )); then
+echo "role: $role"
+echo "token: $token"
+echo "blue_route: $blue_route"
+echo "gold_route: $gold_route"
+echo "blue_subnet: $blue_subnet"
+echo "gold_subnet: $gold_subnet"
+echo "server_ip: $server_ip"
+echo "client_id: $client_id"
+
+if [ $role == "arbiter" ]; then
   # Fetch bundles
   mkdir -p $DIR/blue $DIR/gold
   curl -X GET --header 'Accept: application/json' --header "authorization: $token" $blue_route > $DIR/blue/bundle.tgz
@@ -37,24 +51,21 @@ if (( $role == "arbiter" )); then
   $DIR/blue/start_vpn.bash blue $blue_subnet openvpn.conf
   cd $DIR/gold
   $DIR/gold/start_vpn.bash blue $gold_subnet openvpn.conf
-elif (( $role == "payload" )); then
+elif [ $role == "payload" ]; then
   # Fetch bundle
   mkdir -p $DIR/vpn
-  curl -X GET --header 'Accept: application/json' --header "authorization: $token" $blue_route > $DIR/blue/bundle.tgz
-  curl -X GET --header 'Accept: application/json' --header "authorization: $token" $gold_route > $DIR/gold/bundle.tgz
+  echo curl -X GET --header 'Accept: application/json' --header "authorization: $token" "${client_route}?serverIp=${server_ip}&id=${client_id}"
+  curl -X GET --header 'Accept: application/json' --header "authorization: $token" "${client_route}?serverIp=${server_ip}&id=${client_id}" > $DIR/vpn/bundle.tgz
   
-  # Unpack bundles
-  cd $DIR/blue
-  tar xf bundle.tgz
-  cd $DIR/gold
+  # Unpack bundle
+  cd $DIR/vpn
   tar xf bundle.tgz
   
-  # Start servers
-  cd $DIR/blue
-  $DIR/blue/start_vpn.bash blue $blue_subnet openvpn.conf
-  cd $DIR/gold
-  $DIR/gold/start_vpn.bash blue $gold_subnet openvpn.conf
-
+  # Start server
+  echo cd $DIR/vpn
+  cd $DIR/vpn
+  echo openvpn --config openvpn.conf --daemon
+  openvpn --config openvpn.conf --daemon
 else
   echo "ERROR: Unknown role \"$role\"."
 fi
